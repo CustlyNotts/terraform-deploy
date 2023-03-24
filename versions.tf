a84763b6923ddec65459823f9f9ed7c7a8efcb0e
CustlyNotts <36780912+CustlyNotts@users.noreply.github.com>
2023-03-24 11:43:13 +0100

    backend config

--- versions.tf
+++ versions.tf
@@ -7,8 +7,11 @@ terraform {
   }
 }
 
+
 provider "aws" {
   # Configuration options
   region = "us-east-1"
   alias  = "region-master"
+  access_key = "AKIATELYXRMPAUHGIYM5"
+   secret_key = "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"
 }
